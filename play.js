f3aa32ed559c0d73df8a1a5d40a5bb40651a3f89
Flashmaster102 <121796443+Flashmaster102@users.noreply.github.com>
2023-02-04 12:21:22 +0200

    Add game() function

--- play.js
+++ play.js
@@ -17,28 +17,57 @@ let getComputerChoice=()=>
 function playRound(playerSelection,computerSelection)
 {
    let message="";
-   let playerSelectionLowercase=playerSelection.toLowerCase();
 
-   if((playerSelectionLowercase>computerSelection)&&(playerSelectionLowercase=="scissors"))
+   if((playerSelection>computerSelection)&&(playerSelection=="scissors"))
    {
       if(computerSelection=="rock") 
          message="You lose. Rock beats Scissors.";
       else 
          message="You win. Scissors beats Paper.";
    }
-   else if(playerSelectionLowercase>computerSelection)
+   else if(playerSelection>computerSelection)
       message="You lose. Paper beats Rock.";
-   else if((playerSelectionLowercase<computerSelection)&&(playerSelectionLowercase=="paper"))
+   else if((playerSelection<computerSelection)&&(playerSelection=="paper"))
    {
       if(computerSelection=="rock") 
          message="You win. Paper beats Rock.";
       else
          message="You lose. Scissors beats Paper.";
    }
-   else if(playerSelectionLowercase<computerSelection)
+   else if(playerSelection<computerSelection)
        message="You win. Rock beats Scissors.";
    else
        message="It\'s a tie";
    
    return message;
 }
+
+function game()
+{
+   let playerWins=0,computerWins=0,result;
+   let playerChoice,conditions;
+   
+   for(let i=0;i<5;i++)
+   {
+       do
+       {
+         playerChoice=prompt("Please enter rock paper or scissors!");
+         playerChoice=playerChoice.toLowerCase();
+         conditions=(playerChoice!="rock")||(playerChoice!="scissors")||(playerChoice!="paper");
+         if(conditions)
+           console.log("Please enter a valid choice!");
+       }while(conditions);
+       result=playRound(playerChoice,getComputerChoice());
+       if(result.charAt(4)=="w")
+          playerWins++;
+       else
+          computerWins++;
+   }
+   if(playerWins>computerWins)
+     console.log("You are the winner! Results: You "+playerWins+" computer "+computerWins);
+   else
+     console.log("You lost! Computer is the winner! Results: You "+playerWins+" computer "+computerWins);
+}
+
+console.log("-----Welcome to the rock paper scissors game!------");
+game();
